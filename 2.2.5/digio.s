31bb86d4a5d90b26b4c1b25024cf1f8b13a20562
th3sh3ph3rd <jan.nausner@gmail.com>
2018-10-06 16:54:50 +0200

    implemented 2.2.5 - not tested

---
+++ 2.2.5/digio.s
@@ -0,0 +1,37 @@
+; digio: basic digital I/O
+
+.NOLIST
+.INCLUDE "m1280def.inc"
+.LIST
+
+; Temp registers
+.equ temp0, 0x10
+.equ temp1, 0x11
+.equ temp2, 0x12
+
+; Flash memory
+.section .text
+
+.global main
+.org 0x0000
+    rjmp   main
+
+; This is the main program.
+main: 
+    ldi     temp0, (1<<3)
+    out     DDRA, temp0
+    out     DDRB, temp0
+    out     DDRC, temp0
+    out     DDRB, temp0
+
+    sbi     PORTA, 3
+    in      temp0, PINA
+    in      temp1, PINA
+    in      temp2, PINA
+
+    out     PORTB, temp0
+    out     PORTC, temp0
+    out     PORTD, temp0
+
+loop:
+    rjmp    loop
