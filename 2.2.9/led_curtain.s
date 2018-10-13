518dae79f45a49150453eec36d369dbb478d3efb
th3sh3ph3rd <jan.nausner@gmail.com>
2018-10-13 17:53:26 +0200

    refined led curtain

--- 2.2.9/led_curtain.s
+++ 2.2.9/led_curtain.s
@@ -4,20 +4,21 @@
 .INCLUDE "m1280def.inc"
 .LIST
 
+; make lower i/o fit for lds/sts
+.equ PORTA, PORTA+0x20
+.equ PORTB, PORTB+0x20
+.equ PORTC, PORTC+0x20
+.equ PORTD, PORTD+0x20
+.equ PORTE, PORTE+0x20
+.equ PORTF, PORTF+0x20
+.equ PORTG, PORTG+0x20
+
 ; Temp registers
 .equ temp, 0x10
 .equ port, 0x11
 .equ state, 0x12
-.equ help0, 0x13
-.equ help1, 0x14
-
-;.macro update_port
-;    in      @0, @2
-;    lsl     @0
-;    cpse    @1, 0x01
-;    inc     @0
-;    out     @2, @0
-;.endm
+.equ port_state, 0x13
+.equ save, 0x14
 
 .section .text
 
@@ -29,7 +30,41 @@
 
 .text
 
+update_port: ; port_state contains current PORT value at return
+    push    YH
+    push    YL
+    push    ZH
+    push    ZL
+    push    temp
+
+    ; point Z to the SP
+    in      YH, SPH
+    in      YL, SPL
+
+    ; point Z to the argument
+    adiw    YL, 5 + 2 + 2
+
+    ld      ZL, Y
+    ld      ZH, -Y
+
+    ld      port_state, Z
+    lsl     port_state
+    cpi     state, 1
+    breq    no_inc
+    inc     port_state
+no_inc:
+    st      Z, port_state
+
+    pop     temp
+    pop     ZL
+    pop     ZH
+    pop     YL
+    pop     YH
+ 
+    ret
+
 ; This is the main program.
+; TODO extend to H-L
 main: 
     ; init stack pointer
     ldi     temp, lo8(RAMEND)
@@ -39,13 +74,13 @@ main:
     
     ; put leds in initial state
     ldi     temp, 0x00
-    out     PORTA, temp
-    out     PORTB, temp
-    out     PORTC, temp
-    out     PORTD, temp
-    out     PORTE, temp
-    out     PORTF, temp
-    out     PORTG, temp
+    sts     PORTA, temp
+    sts     PORTB, temp
+    sts     PORTC, temp
+    sts     PORTD, temp
+    sts     PORTE, temp
+    sts     PORTF, temp
+    sts     PORTG, temp
     
     ; set A-G as output
     ldi     temp, 0xff
@@ -58,9 +93,6 @@ main:
     out     DDRG, temp
 
     ; configure timer 1
-    ; set to ctc mode
-    ;ldi     temp, (1<<WGM12)
-    ;sts     TCCR1B, temp
     ; set output compare register A
     ldi     temp, lo8(1250)
     sts     OCR1AL, temp
@@ -80,14 +112,8 @@ main:
     sts     TCCR1B, temp
 
     ; init variables
-    ldi     port, 0
+    ldi     port, PORTA
     ldi     state, 0x00
-
-    ;ldi     temp, 0xff
-    ;out     PORTA, temp
-
-    ldi     help0, 0
-    ldi     help1, 1
 
     ; enable interrupts
     sei
@@ -96,90 +122,38 @@ loop:
     sleep
     rjmp    loop
 
-isr:
-    in      PORTA, temp
-    com     temp
-    out     PORTA, temp
-    reti
-
 tmrisr:
-    cpi     port, 6
-    breq    portg
-    cpi     port, 5
-    breq    portf
-    cpi     port, 4
-    breq    porte
-    cpi     port, 3
-    breq    portd
-    cpi     port, 2
-    breq    portc
-    cpi     port, 1
-    breq    portb
-porta:
-    in      temp, PORTA
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTA, temp
-    cpi     temp, 0x00
+    in      save, SREG
+
+    push    port
+    clr     temp
+    push    temp
+    rcall   update_port
+    pop     temp
+    pop     temp
+    
+    cpi     port, PORTA
+    brne    next_port
+    cpi     port_state, 0x00
     breq    reset_state
-    cpi     temp, 0xff
+    cpi     port_state, 0xff
     breq    set_state
     rjmp    next_port
+
 reset_state:
     ldi     state, 0x00
     rjmp    next_port
 set_state:
     ldi     state, 0x01
-    rjmp    next_port
-portb:
-    in      temp, PORTB
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTB, temp
-    rjmp    next_port
-portc:
-    in      temp, PORTC
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTC, temp
-    rjmp    next_port
-portd:
-    in      temp, PORTD
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTD, temp
-    rjmp    next_port
-porte:
-    in      temp, PORTE
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTE, temp
-    rjmp    next_port
-portf:
-    in      temp, PORTF
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTF, temp
-    rjmp    next_port
-portg:
-    in      temp, PORTG
-    lsl     temp
-    cpse    state, help1
-    inc     temp
-    out     PORTG, temp
-next_port:
-    cpi     port, 6
-    breq    reset_port
-    inc     port
-    reti
-reset_port:
-    ldi     port, 0x00
-    reti
 
+next_port:
+    cpi     port, PORTF
+    breq    rst_port
+    subi    port, -3
+    out     SREG, save
+    reti
+rst_port:
+    ldi     port, PORTA
+    out     SREG, save
+    reti
 
