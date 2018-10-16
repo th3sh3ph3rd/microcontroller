575f6a3dfb73508e8fe55023f07256d0b60c093c
th3sh3ph3rd <jan.nausner@gmail.com>
2018-10-16 21:10:35 +0200

    implemented exam task2

--- exam1/task2/task2.s
+++ exam1/task2/task2.s
@@ -4,6 +4,8 @@
 .LIST
 
 .equ    temp, 0x10
+.equ    state, 0x11
+.equ    save, 0x12
 
 .section .text
 .global main
@@ -11,10 +13,38 @@
 .org 0x00
     rjmp main
 
-.org INT7addr*2
+.org INT6addr*2
     rjmp    isr
 
 isr:
+    in      save, SREG
+    push    temp
+
+    in      temp, PORTB
+    cpi     state, 1
+    breq    sr
+sl:
+    lsl     temp
+    out     PORTB, temp
+    cpi     temp, (1<<PE3)
+    brne    end
+    ldi     state, 1
+    ldi     temp, (1<<ISC71)|(1<<ISC70)
+    sts     EICRB, temp
+    rjmp    end
+sr:
+    lsr     temp
+    out     PORTB, temp
+    cpi     temp, (1<<PE0)
+    brne    end
+    ldi     state, 0
+    ldi     temp, (1<<ISC71)
+    sts     EICRB, temp
+
+end:
+    pop     temp
+    out     SREG, save
+    reti
 
 main:
     ; setup LEDs
@@ -24,6 +54,23 @@ main:
     out      DDRB, temp
 
     ; setup INT7
+    ; enable pullup for INT7
+    ldi     temp, (1<<PE7)
+    out     PORTE, temp
+    ; set falling edge
+    ldi     temp, (1<<ISC71)
+    sts     EICRB, temp
+    ; enable INT7
+    ldi     temp, (1<<INT7)
+    out     EIMSK, temp
+
+    ; init state
+    ldi     state, 0
+    ldi     temp, 0x01
+    out     PORTB, temp
+
+    ; enable interrupts
+    sei
 
 infinite_loop:
     rjmp    infinite_loop
