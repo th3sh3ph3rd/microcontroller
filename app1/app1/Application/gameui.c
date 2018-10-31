98518a5435312ea216cd9cdda27f9217b74a8322
th3sh3ph3rd <jan.nausner@gmail.com>
2018-10-31 12:10:30 +0100

    flatted gameui fsm

--- app1/app1/Application/gameui.c
+++ app1/app1/Application/gameui.c
@@ -13,18 +13,21 @@
 
 #define WALL_GAP   12
 
+enum gt_state {IDLE, SCROLL, LEVEL};
+
 static uint8_t walls = {{0, 26, 50, 57, 82, 109}};
 
 /* State variables */
-uint8_t yShift = 0;
+static uint8_t y_shift = 0;
+static enum gt_state gametick_state = DONE;
 
 /* Counter variables */
-uint8_t gameTicksScroll = 0;
-uint8_t gameTicksPerScroll = 10;
-uint8_t scrollTicks = 0;
+static uint8_t gameTicksScroll = 0;
+static uint8_t gameTicksPerScroll = 10;
+static uint8_t scrollTicks = 0;
 
 /* Local functions */
-void displayNewWall(void);
+static void displayNewWall(void);
 
 /**
  * @brief       Initialize the game user interface.
@@ -46,7 +49,7 @@ uint8_t gameui_setup(game_state_t *game_state)
         y_off += WALL_GAP+1;
     }
 
-    yShift = glcdSetYShift();
+    y_shift = glcdSetYShift();
 
     *game_state = RUNNING;
 
@@ -60,13 +63,27 @@ uint8_t gameui_setup(game_state_t *game_state)
  * @return              The function returns a non-zero value if there is still something to do and 0 if
  *                      all tasks of one tick have been completed.
  */
+//TODO for tasking return in every block
 uint8_t gameui_tick(game_state_t *game_state)
 {
-    if (gameTicksScroll == gameTicksPerScroll)
+    if (IDLE == gametick_state)
+    {
+        if (gameTicksScroll == gameTicksPerScroll)
+        {
+            gametick_state = SCROLL;
+            gameTicksScroll = 0;
+        }
+        else
+        {
+            gameTicksScroll++;
+        }
+    }
+    
+    if (SCROLL == gametick_state)
     {
         if (scrollTicks == WALL_GAP)
         {
-            displayNewWall(0);
+            gametick_state = LEVEL;
             scrollTicks = 0;
         }
         else
@@ -74,20 +91,23 @@ uint8_t gameui_tick(game_state_t *game_state)
             scrollTicks++;
         }
 
-        glcdSetYShift(++yShift);
-        gameTicksScroll = 0;
+        glcdSetYShift(++y_shift);
     }
-    else
+
+    if (LEVEL == gametick_state)
     {
-        gameTicksScroll++;
+        gametick_state = IDLE;
+        displayNewWall(0);
     }
+
+    return IDLE == gametick_state ? 0 : 1;
 }
 
 //uint8_t gameui_pause(game_state_t *game_state);
 
 //uint8_t gameui_reconnect(game_state_t *game_state);
 
-void displayNewWall(uint8_t y_off)
+static void displayNewWall(uint8_t y_off)
 {
     xy_point_t point0, point1;
 
