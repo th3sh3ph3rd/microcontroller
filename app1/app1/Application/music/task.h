0053e45b80d452ffabfd93827b797e08330b635c
th3sh3ph3rd <jan.nausner@gmail.com>
2018-11-20 01:23:27 +0100

    new makefile almost working

---
+++ app1/app1/Application/music/task.h
@@ -0,0 +1,17 @@
+/**
+ *
+ * @file task.h
+ * @author Jan Nausner <e01614835@student.tuwien.ac.at>
+ * @date 2018-11-16
+ *
+ * Provide a task function return type.
+ *
+ */
+
+#ifndef __TASK__
+#define __TASK__
+
+typedef enum {DONE, BUSY} task_state_t;
+
+#endif
+
